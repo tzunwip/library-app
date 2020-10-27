764858287fc8a9286a83b2e0f67d42ccc6f90d7d
Tzun Wong <tzun.wong@gmail.com>
2020-10-27 16:14:39 +0800

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = () => `${title} by ${author}, ${pages} pages, ${read} yet.`;
+}
