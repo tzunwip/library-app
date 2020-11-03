abe9b0b378d22492262020c9a4aa858e78871430
Tzun Wong <tzun.wong@gmail.com>
2020-11-03 23:20:23 +0800

    improved bookid assignment

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,17 @@
-let myLibrary = [];
-let bookIdCounter = 0;
+let myLibrary = [
+  {title: "The Bitcoin Standard: The Decentralized Alternative to Central Banking", author: "Saifedean Ammous", pages: "304", read: true, bookid: 1},
+  {title: "The Internet of Money: A collection of talks", author: "Andreas M. Antonopoulos", pages: "152", read: false, bookid: 2},
+  {title: "Digital Gold: Bitcoin and the Inside Story of the Misfits and Millionaires Trying to Reinvent Money", author: "Nathaniel Popper", pages: "432", read: false, bookid: 3},
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.bookid = bookIdCounter++;
+  this.bookid = myLibrary.length == 0 ? 1 : myLibrary[myLibrary.length - 1].bookid + 1;
 }
 
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
-
-addBookToLibrary("The Bitcoin Standard: The Decentralized Alternative to Central Banking", "Saifedean Ammous", "304", true);
-addBookToLibrary("The Internet of Money: A collection of talks", "Andreas M. Antonopoulos", "152", false);
-addBookToLibrary("Digital Gold: Bitcoin and the Inside Story of the Misfits and Millionaires Trying to Reinvent Money", "Nathaniel Popper", "432", false);
