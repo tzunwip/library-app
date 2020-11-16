eb223a8ef48f2eec1e1a3ff5025e5e7dc9edcd8d
Tzun Wong <tzun.wong@gmail.com>
2020-11-17 01:18:51 +0800

    added edit button

--- js/form.js
+++ js/form.js
@@ -1,7 +1,6 @@
 function generateAddBookForm() {
   const canvas = document.createElement("div");
   canvas.setAttribute("class", "canvas");
-
   
   const formContainer = document.createElement("form");
   formContainer.setAttribute("class", "form");
@@ -17,7 +16,31 @@ function generateAddBookForm() {
     {text: "Yes", value: "true"},
     {text: "No", value: "false"},
   ]);
-  createFormControls(formContainer);
+  createFormControls(formContainer, "Add");
+
+  canvas.appendChild(formContainer);
+  body.appendChild(canvas);
+}
+
+function generateEditBookForm(targetBook) {
+  const canvas = document.createElement("div");
+  canvas.setAttribute("class", "canvas");
+  
+  const formContainer = document.createElement("form");
+  formContainer.setAttribute("class", "form");
+  formContainer.setAttribute("id", "editBookForm");
+  formContainer.setAttribute("autocomplete", "off");
+  formContainer.addEventListener("submit", (e) => formEditBookSubmit(e, targetBook.bookid));
+
+  createCloseButton(formContainer);
+  createTypedInput(formContainer, "title", "text", targetBook.title);
+  createTypedInput(formContainer, "author", "text", targetBook.author);
+  createTypedInput(formContainer, "pages", "number", targetBook.pages);
+  createSelectInput(formContainer, "read", [
+    {text: "Yes", value: true},
+    {text: "No", value: false},
+  ], targetBook.read);
+  createFormControls(formContainer, "Edit");
 
   canvas.appendChild(formContainer);
   body.appendChild(canvas);
@@ -34,7 +57,7 @@ function createCloseButton(parent) {
   parent.appendChild(formClose);
 }
 
-function createTypedInput(parent, name, type) {
+function createTypedInput(parent, name, type, placeholder = "") {
   const inputContainer = document.createElement("div");
   inputContainer.setAttribute("class", `form__input ${name}__container`);
 
@@ -50,12 +73,13 @@ function createTypedInput(parent, name, type) {
   inputField.setAttribute("name", `${name}`);
   inputField.setAttribute("type", `${type}`);
   inputField.setAttribute("required", "");
+  inputField.setAttribute("value", placeholder);
   inputContainer.appendChild(inputField);
 
   parent.appendChild(inputContainer);
 }
 
-function createSelectInput(parent, name, options) {
+function createSelectInput(parent, name, options, selectedValue) {
   const inputContainer = document.createElement("div");
   inputContainer.setAttribute("class", `form__input ${name}__container`);
   
@@ -75,6 +99,9 @@ function createSelectInput(parent, name, options) {
     
     optionField.setAttribute("value", obj.value);
     optionField.textContent = obj.text;
+    if (selectedValue == obj.value) {
+      optionField.setAttribute("selected", "true");
+    };
 
     inputField.appendChild(optionField);
   });
@@ -83,14 +110,14 @@ function createSelectInput(parent, name, options) {
   parent.appendChild(inputContainer);
 }
 
-function createFormControls(parent) {
+function createFormControls(parent, submitButtonText = "Submit") {
   const buttonContainer = document.createElement("div");
   buttonContainer.setAttribute("class", `form__control control__container`);
 
   const submitButton = document.createElement("button");
   submitButton.setAttribute("class", "form__button form__submit");
   submitButton.setAttribute("type", "submit");
-  submitButton.textContent = "Add";
+  submitButton.textContent = submitButtonText;
   buttonContainer.appendChild(submitButton);
 
   const resetButton = document.createElement("button");
@@ -105,7 +132,6 @@ function createFormControls(parent) {
 function formAddBookSubmit(e) {
   e.preventDefault();
 
-  const formName = e.target.id;
   const formElements = e.target.elements;
   const title = formElements.title.value;
   const author = formElements.author.value;
@@ -117,7 +143,23 @@ function formAddBookSubmit(e) {
   e.target.reset();
   refreshDisplay();
   alert(`${title} by ${author} was added.`);
-  }
+}
+
+function formEditBookSubmit(e, targetBookId) {
+  e.preventDefault();
+
+  const formElements = e.target.elements;
+  const title = formElements.title.value;
+  const author = formElements.author.value;
+  const pages = formElements.pages.value;
+  const read = formElements.read.value;
+
+  editBook(targetBookId, title, author, pages, read);
+
+  refreshDisplay();
+  alert(`${title} by ${author} was edited.`);
+  document.querySelector(".canvas").remove();
+}
 
 // utility functions
 function capitalizeFirstChar(str) {
